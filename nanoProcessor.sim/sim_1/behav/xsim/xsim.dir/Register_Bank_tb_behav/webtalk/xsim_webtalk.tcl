784b6abae2e41ce43503fbfa51b09552005d3d07
Nadeesha Jayamanne <nadeeshajayamanne@protonmail.com>
2025-05-15 16:14:33 +0530

    8th commit

--- nanoProcessor.sim/sim_1/behav/xsim/xsim.dir/Register_Bank_tb_behav/webtalk/xsim_webtalk.tcl
+++ nanoProcessor.sim/sim_1/behav/xsim/xsim.dir/Register_Bank_tb_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir D:/Github repository/NanoProcessor/nanoProcessor.sim/sim_1/behav/xsim/xsim.dir/Register_Bank_tb_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Thu May 15 16:07:39 2025" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Thu May 15 16:14:21 2025" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2018.1 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2188600" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "33f2bbb6-ad18-4cea-aa6f-54fb5bd47a18" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "2ee2a8ca29e346d1b7d85f00cd73aaff" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "17" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "18" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Microsoft Windows 8 or later , 64-bit" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "11th Gen Intel(R) Core(TM) i7-1165G7 @ 2.80GHz" -context "user_environment"
@@ -27,6 +27,6 @@ webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\
 webtalk_add_data -client xsim -key runtime -value "1 us" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "5" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Simulation_Time -value "0.06_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "8204_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 1942715055 -regid "" -xml D:/Github repository/NanoProcessor/nanoProcessor.sim/sim_1/behav/xsim/xsim.dir/Register_Bank_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html D:/Github repository/NanoProcessor/nanoProcessor.sim/sim_1/behav/xsim/xsim.dir/Register_Bank_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm D:/Github repository/NanoProcessor/nanoProcessor.sim/sim_1/behav/xsim/xsim.dir/Register_Bank_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Memory -value "8196_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 2990244195 -regid "" -xml D:/Github repository/NanoProcessor/nanoProcessor.sim/sim_1/behav/xsim/xsim.dir/Register_Bank_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html D:/Github repository/NanoProcessor/nanoProcessor.sim/sim_1/behav/xsim/xsim.dir/Register_Bank_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm D:/Github repository/NanoProcessor/nanoProcessor.sim/sim_1/behav/xsim/xsim.dir/Register_Bank_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
--- nanoProcessor.srcs/sim_1/imports/new/Register_Bank_tb.vhd
+++ nanoProcessor.srcs/sim_1/imports/new/Register_Bank_tb.vhd
@@ -1,36 +1,7 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 05/11/2025 09:41:57 AM
--- Design Name: 
--- Module Name: Register_Bank_tb - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 use work.Reg_types.all;
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity Register_Bank_tb is
 --  Port ( );
